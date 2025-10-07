d125bbab2fee3cfa8c4a8e913b126a8e0dedb949
guillermosoriacorrea <guillermo.soria@tiendamia.com>
2025-10-07 20:43:30 -0300

    refactor: remove unused import for AvailabilityQuery in routes

--- src/adapters/http/routes.ts
+++ src/adapters/http/routes.ts
@@ -4,7 +4,6 @@ import { readyController } from "./controllers/ready.controller";
 import { getAvailableRoomsController } from "./controllers/availability.controller";
 import { createReservationController } from "./controllers/reservations.controller";
 import { validate } from "./validate";
-import { AvailabilityQuery } from "./schemas/availability.schema";
 import { ReservationBody } from "./schemas/reservation.schema";
 import { AvailabilityQuerySchema } from "./schemas/availability.schema";
 
